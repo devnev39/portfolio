15b11d566df2f89ba1be7499ae29862560be9ff9
Bhuvanesh Bonde <devnev39@gmail.com>
2026-03-09 08:51:19 +0530

    fix: profile fix

--- src/types/content.types.ts
+++ src/types/content.types.ts
@@ -177,6 +177,8 @@ export interface About extends BasePageConfig {
       name: string;
       /** Description of studies */
       description: React.ReactNode;
+      //** Percentage or marks achieved */
+      percentage: string;
     }>;
   };
   /** Technical skills section */
